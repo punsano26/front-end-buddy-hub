56aee4ce4144c15346968a579aa38519436c4300
bank_npru <punsan.somkla@gmail.com>
2026-03-06 01:07:52 +0700

    feat/simplify avatar rendering in ToolsBar component

--- app/components/app/ToolsBar.vue
+++ app/components/app/ToolsBar.vue
@@ -46,12 +46,11 @@
             class="w-6 h-6"
             src="/svg/mdi-light--bell.svg">
 
-          <OverlayBadge>
-            <Avatar
-              icon="pi pi-user"
-              image="https://primefaces.org/cdn/primevue/images/avatar/amyelsner.png"
-              size="large" />
-          </OverlayBadge>
+
+          <Avatar
+            icon="pi pi-user"
+            image="https://primefaces.org/cdn/primevue/images/avatar/amyelsner.png"
+            size="large" />
         </div>
       </template>
     </Toolbar>
