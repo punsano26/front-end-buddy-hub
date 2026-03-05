5ef82d1431191d257a005bc4fcbe8ad5c4013d20
bank_npru <punsan.somkla@gmail.com>
2026-03-05 18:39:53 +0700

    feat/add Avatar, Badge, ContrastButton, SecondaryButton, and Toolbar components with theme customization

---
+++ app/volt/OverlayBadge.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="relative inline-flex">
+    <slot />
+    <Badge
+      v-bind="$attrs"
+      class="absolute top-0 end-0 translate-x-[50%] translate-y-[-50%] origin-[100%_0] m-0 outline-2 outline-surface-0 dark:outline-surface-900" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import Badge from './Badge.vue'
+import type { BadgeProps } from 'primevue/badge'
+
+interface Props extends /* @vue-ignore */ BadgeProps {}
+defineProps<Props>()
+
+defineOptions({
+  inheritAttrs: false
+})
+</script>
