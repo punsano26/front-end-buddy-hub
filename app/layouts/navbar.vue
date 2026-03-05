f4ce017807795aaaec245766d1a373770e463156
bank_npru <punsan.somkla@gmail.com>
2026-03-05 18:40:11 +0700

    feat/update default layout with full-screen grid and add navbar component

---
+++ app/layouts/navbar.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <ToolsBar />
+    <slot />
+  </div>
+</template>
+
+<script setup lang="ts">
+import ToolsBar from '~/components/app/ToolsBar.vue'
+</script>
+
+<style scoped>
+
+</style>
