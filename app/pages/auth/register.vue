b32c44db6cd1e60752a18ca9eb8573c7effb0f63
Punsano26 <654259026@webmail.npru.ac.th>
2026-03-14 23:13:47 +0700

    feat/create register page with form fields and upload image profile component for integrate with backend API

--- app/pages/auth/register.vue
+++ app/pages/auth/register.vue
@@ -1,11 +1,44 @@
 <template>
-  <div>
-    <h1>hello register now!</h1>
-  </div>
+  <form>
+    <UploadImageProfile />
+    <InputLabelField
+      label="อีเมล"
+      placeholder="อีเมลของคุณ" />
+    <InputLabelField
+      label="ชื่อผู้ใช้"
+      placeholder="ชื่อผู้ใช้ของคุณ" />
+    <InputLabelField
+      label="เพศ">
+      <Select
+        class="w-full"
+        placeholder="เลือกเพศของคุณ" />
+    </InputLabelField>
+    <InputLabelField label="วันเกิด">
+      <DatePicker
+        class="w-full"
+        placeholder="เลือกวันเกิดของคุณ" />
+    </InputLabelField>
+    <InputLabelField label="รหัสผ่าน">
+      <InputPasswordField />
+    </InputLabelField>
+    <InputLabelField label="ยืนยันรหัสผ่าน">
+      <InputPasswordField />
+    </InputLabelField>
+    <Button
+      label="สมัครสมาชิก"
+      pt:label:class="font-bold"
+      pt:root:class="bg-gradient-primary border-none rounded-xl py-3"
+      type="submit" />
+  </form>
 </template>
 
 <script setup lang="ts">
+import InputLabelField from '~/components/input/InputLabelField.vue'
+import UploadImageProfile from '~/components/input/UploadImageProfile.vue'
 
+definePageMeta({
+  title: 'สมัครสมาชิก'
+})
 </script>
 
 <style scoped>
