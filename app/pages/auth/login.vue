534216a7df6e63937ff220b2a0c2f8dc1c0b00b1
Punsano26 <654259026@webmail.npru.ac.th>
2026-03-15 17:06:09 +0700

    feat/bind password input to form model and enhance login functionality for integrated authentication flow

--- app/pages/auth/login.vue
+++ app/pages/auth/login.vue
@@ -6,7 +6,7 @@
       disabled
       required />
     <InputLabelField label="รหัสผ่าน">
-      <InputPasswordField />
+      <InputPasswordField v-model="form.password" />
     </InputLabelField>
     <Button
       label="เข้าสู่ระบบ"
@@ -24,17 +24,38 @@
 <script setup lang="ts">
 import InputLabelField from '~/components/input/InputLabelField.vue'
 import InputPasswordField from '~/components/input/InputPasswordField.vue'
+import type { IAuthLoginPayload } from '~/models/request/AuthReq.model'
 
+// import type { IAuthProvider } from '~/resource/provider/Auth.provider'
+// import AuthProvider from '~/resource/provider/Auth.provider'
+
+// const authService: IAuthProvider = new AuthProvider()
 const route = useRoute()
+// const router = useRouter()
+// const authStore = useAuthStore()
 
 definePageMeta({
   title: 'เข้าสู่ระบบเพื่อใช้งาน'
 })
 
-const form = ref<any>({
-  account: route?.query?.account || '',
+const form = ref<IAuthLoginPayload>({
+  account: Array.isArray(route.query.account)
+    ? route.query.account[0] ?? ''
+    : route.query.account ?? '',
   password: ''
 })
+
+// async function onLogin (): Promise<void> {
+//   if (!form.value.account || !form.value.password) return
+//   const payload = {
+//     account: form.value.account,
+//     password: form.value.password
+//   }
+//   const response = await authService.login(payload)
+//   authStore.userLogin(response.data, response.token, response.refreshToken)
+
+//   router.push({ name: 'admin-order' })
+// }
 </script>
 
 <style scoped>
