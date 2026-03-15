55ebf6ee128232945ed9009e0e52fdb8e98fadfd
Punsano26 <654259026@webmail.npru.ac.th>
2026-03-15 17:04:55 +0700

    feat/add login method to AuthProvider for user authentication

--- app/resource/provider/Auth.provider.ts
+++ app/resource/provider/Auth.provider.ts
@@ -1,9 +1,10 @@
 import HttpRequest from '../HttpRequest'
-import type { ICheckAuthPayload } from '~/models/request/AuthReq.model'
-import type { ICheckAuthResponse } from '~/models/response/AuthRes.model'
+import type { IAuthLoginPayload, ICheckAuthPayload } from '~/models/request/AuthReq.model'
+import type { IAuthLoginResponse, ICheckAuthResponse } from '~/models/response/AuthRes.model'
 
 export interface IAuthProvider {
   checkAuth (payload: ICheckAuthPayload): Promise<ICheckAuthResponse>
+  login (payload: IAuthLoginPayload): Promise<IAuthLoginResponse>
 }
 
 class AuthProvider extends HttpRequest implements IAuthProvider {
@@ -11,6 +12,11 @@ class AuthProvider extends HttpRequest implements IAuthProvider {
 
   public async checkAuth (payload: ICheckAuthPayload): Promise<ICheckAuthResponse> {
     const response = await this.post(`${this.urlPrefix}/checkUser`, payload)
+    return response
+  }
+
+  public async login (payload: IAuthLoginPayload): Promise<IAuthLoginResponse> {
+    const response = await this.post(`${this.urlPrefix}/login`, payload)
     return response
   }
 }
