2bfa7644f6eeeb092e09a9cdcfece6197dc485a5
Punsano26 <654259026@webmail.npru.ac.th>
2026-03-19 22:57:52 +0700

    feat/AvatarProfile: Add AvatarProfile component and integrate into ToolsBar

---
+++ app/components/app/AvatarProfile.vue
@@ -0,0 +1,51 @@
+<template>
+  <Button
+    aria-controls="overlay_menu"
+    aria-haspopup="true"
+    pt:root:class="p-0 border-none bg-transparent"
+    plain
+    @click="toggle">
+    <Avatar
+      icon="pi pi-user"
+      image="https://primefaces.org/cdn/primevue/images/avatar/amyelsner.png"
+      size="large" />
+  </Button>
+  <Menu
+    id="overlay_menu"
+    ref="menu"
+    :model="items"
+    :popup="true" />
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+import Button from '~/volt/Button.vue'
+import Menu from '~/volt/Menu.vue'
+
+interface IMenuItem {
+  label?: string
+  icon?: string
+  items?: IMenuItem[]
+}
+
+const menu = ref()
+const items = ref<IMenuItem[]>([
+  {
+    label: `username`,
+    items: [
+      {
+        label: 'logout',
+        icon: 'pi pi-refresh'
+      }
+    ]
+  }
+])
+
+const toggle = (event: Event): void => {
+  menu.value?.toggle(event)
+}
+</script>
+
+<style scoped>
+
+</style>
