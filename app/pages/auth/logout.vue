fc8b0c8bb4de98e1e42a9f93cf86dfa1fd3e3dc5
Punsano26 <654259026@webmail.npru.ac.th>
2026-03-20 10:06:09 +0700

    feat/integrate auth provider for logout functionality

--- app/pages/auth/logout.vue
+++ app/pages/auth/logout.vue
@@ -3,7 +3,32 @@
 </template>
 
 <script setup lang="ts">
+import { useToast } from 'primevue/usetoast'
+import type { IAuthProvider } from '~/resource/provider/Auth.provider'
+import AuthProvider from '~/resource/provider/Auth.provider'
 
+const authService: IAuthProvider = new AuthProvider()
+const router = useRouter()
+const authStore = useAuthStore()
+const toast = useToast()
+const { $handleLoading } = useNuxtApp()
+
+async function onLogout (): Promise<void> {
+  await authService.logout()
+  authStore.logout()
+  router.push({ name: 'public-home' })
+}
+
+onMounted((): void => {
+  $handleLoading(onLogout, {
+    toast: {
+      instance: toast,
+      success: {
+        summary: 'ออกจากระบบสำเร็จ'
+      }
+    }
+  })
+})
 </script>
 
 <style scoped>
