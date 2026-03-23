b41d773fa4b64f841d031da3807983b0687c840b
Punsano26 <654259026@webmail.npru.ac.th>
2026-03-24 00:10:01 +0700

    feat/create header and input for chat room

---
+++ app/components/DirectMessageChatRoom.vue
@@ -0,0 +1,23 @@
+<template>
+  <Card pt:root:class="w-full rounded-none">
+    <template #content>
+      <div class="flex gap-2">
+        <InputLabelField
+          placeholder="พิมพ์ข้อความของคุณนี่ที่..." />
+        <Button
+          class="ml-2"
+          color="primary">
+          ส่ง
+        </Button>
+      </div>
+    </template>
+  </Card>
+</template>
+
+<script setup lang="ts">
+import InputLabelField from './input/InputLabelField.vue'
+</script>
+
+<style scoped>
+
+</style>
