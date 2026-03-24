3a16d8a1186d80c12bac78af92ac679071ffb3e2
Punsano26 <654259026@webmail.npru.ac.th>
2026-03-24 16:57:56 +0700

    feat/integrate user profile with api

--- app/pages/public/profile/index.vue
+++ app/pages/public/profile/index.vue
@@ -4,26 +4,32 @@
       <template #header>
         <img
           class="w-full h-32 object-cover rounded-tl-xl rounded-tr-xl"
-          src="https://picsum.photos/seed/picsum/200/300"
-          alt=""
+          :src="items?.bannerImg || 'https://primefaces.org/cdn/primevue/images/profilebg.png'"
+          alt="user banner"
         />
         <div class="flex gap-4 -mt-12 px-4">
           <img
             alt="user avatar"
             class="w-26 h-26 border-2 rounded-2xl"
-            src="https://primefaces.org/cdn/primevue/images/avatar/amyelsner.png"
+            :src="items?.profileImg || 'https://primefaces.org/cdn/primevue/images/avatar/amyelsner.png'"
           />
         </div>
       </template>
       <template #content>
         <div class="flex flex-col md:flex-row gap-4">
           <div class="flex flex-col gap-2 ">
-            <p class="text-xl font-bold">username</p>
+            <p class="text-xl font-bold">{{ items?.username }}</p>
             <p class="text-sm text-surface-500">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas,
-              voluptate.
+              {{ items?.description }}
             </p>
-            <p>Joined Jan 2025</p>
+              <div class="flex gap-2 items-center">
+
+                <p class="text-sm text-surface-500">Joined {{ items?.createdAt }}</p>
+                <p class="text-sm text-surface-500">{{ items?.email }}</p>
+                <p class="text-sm text-surface-500">{{ items?.gender }}</p>
+                <p class="text-sm text-surface-500">{{ items?.dateOfBirth }}</p>
+                <p class="text-sm text-surface-500">{{ items?.age }}</p>
+              </div>
           </div>
           <div>
             <Button
@@ -53,7 +59,26 @@
 </template>
 
 <script setup lang="ts">
+import type { IFindOneCurrentUserData } from '~/models/response/UserRes.model'
+import UserProvider, { type IUserProvider } from '~/resource/provider/User.provider'
+
 definePageMeta({ layout: "navbar" });
+
+const userService: IUserProvider = new UserProvider()
+const { $handleLoading } = useNuxtApp()
+const items = ref<IFindOneCurrentUserData>()
+async function useFetch (): Promise<void> {
+const  response =  await userService.findOneCurrentUser()
+  items.value = response?.data
+}
+
+function fetch (): void {
+  $handleLoading(useFetch)
+}
+
+onMounted(() => {
+  fetch()
+})
 </script>
 
 <style scoped></style>
