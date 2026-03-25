dc375266e9b0fa7052da987fbfa06d9c03eb5cff
Punsano26 <654259026@webmail.npru.ac.th>
2026-03-25 16:34:28 +0700

    feat/add paginate type in global model

--- app/models/Global.model.ts
+++ app/models/Global.model.ts
@@ -1,3 +1,9 @@
+type TPaginationLimit = 20 | 50 | 100 | 9999
+export interface IPaginationRequest {
+  page?: number
+  limit?: TPaginationLimit
+}
+
 
 export interface IMenuItem {
   label?: string
@@ -8,6 +14,13 @@ export interface IMenuItem {
 export interface IItems {
   label: string
   command: () => void
+}
+
+export interface IPagination extends Omit<IPaginationRequest, 'page' | 'limit'> {
+  page: number
+  limit: TPaginationLimit
+  totalPage: number
+  count: number
 }
 export interface IBaseOptions<T = string | number> {
   label: string
