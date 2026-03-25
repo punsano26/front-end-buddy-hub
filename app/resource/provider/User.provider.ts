fe2335e22c9c1dc3d6a608dc2870bc743835c2a3
Punsano26 <654259026@webmail.npru.ac.th>
2026-03-25 16:33:54 +0700

    feat/add provider method for update user and get user by id

--- app/resource/provider/User.provider.ts
+++ app/resource/provider/User.provider.ts
@@ -1,8 +1,13 @@
 import HttpRequest from '../HttpRequest'
-import type { IFindOneCurrentUserResponse } from '~/models/response/UserRes.model'
+import type { TBaseParamsId } from '~/models/request/Request.model'
+import type { IFindAllUsersPaginateQuery, IUpdateUserPayload } from '~/models/request/UserReq.model'
+import type { IFindAllUsersPaginateResponse, IFindOneCurrentUserResponse } from '~/models/response/UserRes.model'
 
 export interface IUserProvider {
   findOneCurrentUser (): Promise<IFindOneCurrentUserResponse>
+  findAllUsersPaginate (query: IFindAllUsersPaginateQuery): Promise<IFindAllUsersPaginateResponse>
+  findOneUserById (id: TBaseParamsId): Promise<IFindOneCurrentUserResponse>
+  updateUser (payload: IUpdateUserPayload): Promise<IFindOneCurrentUserResponse>
 }
 
 class UserProvider extends HttpRequest implements IUserProvider {
@@ -11,6 +16,23 @@ class UserProvider extends HttpRequest implements IUserProvider {
   public async findOneCurrentUser (): Promise<IFindOneCurrentUserResponse> {
     this.setUserAuthHeader()
     const response = await this.get(`${this.urlPrefix}/myInfo`)
+    return response
+  }
+
+  public async findAllUsersPaginate (query: IFindAllUsersPaginateQuery): Promise<IFindAllUsersPaginateResponse> {
+    const response = await this.get(this.urlPrefix, query)
+    return response
+  }
+
+  public async findOneUserById (id: TBaseParamsId): Promise<IFindOneCurrentUserResponse> {
+    this.setUserAuthHeader()
+    const response = await this.get(`${this.urlPrefix}/${id}`)
+    return response
+  }
+
+  public async updateUser (payload: IUpdateUserPayload): Promise<IFindOneCurrentUserResponse> {
+    this.setUserAuthHeader()
+    const response = await this.patch(`${this.urlPrefix}/update`, payload)
     return response
   }
 }
