15f647ad9421264b0265d200eefa7decd3993498
Punsano26 <654259026@webmail.npru.ac.th>
2026-03-25 16:36:41 +0700

    feat/add request and response models for user pagination and updates

--- app/models/response/UserRes.model.ts
+++ app/models/response/UserRes.model.ts
@@ -1,4 +1,4 @@
-import type { IApiResponse } from './Response.model'
+import type { IApiResponse, IBasePaginationResponse } from './Response.model'
 
 export interface IFindOneCurrentUserData {
   profileImg: string | null
@@ -13,4 +13,16 @@ export interface IFindOneCurrentUserData {
   dateOfBirth: string
 }
 
+export interface IUserList {
+  id: number
+  username: string
+  nickname: string | null
+  profileImg: string | null
+  gender: string
+  dateOfBirth: string
+}
+
 export type IFindOneCurrentUserResponse = IApiResponse<IFindOneCurrentUserData>
+
+
+export interface IFindAllUsersPaginateResponse extends IBasePaginationResponse<IUserList> {}
