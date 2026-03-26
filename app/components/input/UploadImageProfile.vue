87952b986d10f49b3eb9b9fffe13ab840ef4ae6f
Punsano26 <654259026@webmail.npru.ac.th>
2026-03-26 21:12:44 +0700

    feat/add component UploadImageProfile.vue to allow users to upload profile and banner images

--- app/components/input/UploadImageProfile.vue
+++ app/components/input/UploadImageProfile.vue
@@ -1,13 +1,12 @@
 <template>
   <p class="text-sm">
-    อัพโหลดรูปภาพโปรไฟล์
+    {{ props.label }}
   </p>
 
   <!-- Banner -->
   <div class="relative w-full">
     <img
-      :src="bannerPreview || '/png/upload-banner.png'"
-      alt="user banner"
+      :src="bannerPreview || props.value?.bannerImg || '/png/upload-banner.png'"
       class="w-full h-32 object-cover rounded-tl-xl rounded-tr-xl">
 
     <div
@@ -29,13 +28,11 @@
   </div>
 
   <!-- Avatar -->
-  <div class="flex justify-center items-center -mt-18 px-4">
+  <div class="flex -mt-14 px-4">
     <div class="relative w-24 h-24">
       <img
-        :src="avatarPreview || '/png/upload-profile.png'"
-        alt="user avatar"
+        :src="avatarPreview || props.value?.profileImg || '/png/upload-profile.png'"
         class="w-24 h-24 rounded-full border-4 border-surface-0 object-cover">
-
       <div
         class="absolute bottom-0 right-0 w-7 h-7 rounded-full border-2 border-white bg-white flex items-center justify-center cursor-pointer"
         @click="avatarInput?.click()">
@@ -54,17 +51,54 @@
         @change="onAvatarChange">
     </div>
   </div>
+
+  <Button
+    v-if="hasSelectedImage"
+    class="mt-4 text-sm font-bold w-full"
+    pt:root:class="border-none justify-center px-2 rounded-xl"
+    @click="onSubmit">
+    อัปโหลดรูปภาพ
+  </Button>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue'
+import { computed, ref } from 'vue'
+import { useToast } from 'primevue/usetoast'
+import { UploadCategoryEnum } from '~/models/enums/Upload.enum'
+import type { IFindOneCurrentUserData } from '~/models/response/UserRes.model'
+import type { ICreateUploadData } from '~/models/response/๊UploadRes.model'
+import type { IUploadProvider } from '~/resource/provider/Upload.provider'
+import UploadProvider from '~/resource/provider/Upload.provider'
 
 const bannerInput = ref<HTMLInputElement | null>(null)
 const avatarInput = ref<HTMLInputElement | null>(null)
-
+const { $handleLoading } = useNuxtApp()
 const bannerPreview = ref<string>('')
 const avatarPreview = ref<string>('')
+const toast = useToast()
+const UploadService: IUploadProvider = new UploadProvider()
+const hasSelectedImage = computed((): boolean => {
+  return Boolean(bannerPreview.value || avatarPreview.value)
+})
+interface IProps {
+  tag?: 'label' | 'div'
+  label?: string
+  required?: boolean
+  bold?: boolean
+  rules?: ((v: any) => boolean | string)[]
+  showError?: boolean
+  value?: IFindOneCurrentUserData
+}
+const props = withDefaults(defineProps<IProps>(), {
+  tag: 'label',
+  label: undefined,
+  required: false,
+  bold: false
+})
 
+const emit = defineEmits<{
+  update: []
+}>()
 function onBannerChange (event: Event): void {
   const file = (event.target as HTMLInputElement).files?.[0]
   if (!file) return
@@ -75,5 +109,43 @@ function onAvatarChange (event: Event): void {
   const file = (event.target as HTMLInputElement).files?.[0]
   if (!file) return
   avatarPreview.value = URL.createObjectURL(file)
+}
+
+function resetPreviews (): void {
+  bannerPreview.value = ''
+  avatarPreview.value = ''
+}
+
+async function handleUpload (file: File, category: UploadCategoryEnum): Promise<ICreateUploadData> {
+  const response = await UploadService.onUpload({
+    file,
+    category
+  })
+  return response.data
+}
+
+async function useSubmit (): Promise<void> {
+  if (bannerInput.value?.files?.[0]) {
+    await handleUpload(
+      bannerInput.value.files[0], UploadCategoryEnum.BANNER
+    )
+  }
+
+  if (avatarInput.value?.files?.[0]) {
+    await handleUpload(
+      avatarInput.value.files[0], UploadCategoryEnum.PROFILE
+    )
+  }
+  resetPreviews()
+
+  emit('update')
+}
+
+function onSubmit (): void {
+  $handleLoading(useSubmit, {
+    toast: {
+      instance: toast
+    }
+  })
 }
 </script>
