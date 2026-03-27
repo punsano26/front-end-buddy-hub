532e23959a01ac6f574f1c7b22749f5d1053c91c
Punsano26 <654259026@webmail.npru.ac.th>
2026-03-27 23:56:25 +0700

    fix/change logic refetch token and add navigate to verify page if not login when access public page

--- app/middleware/refetch.global.ts
+++ app/middleware/refetch.global.ts
@@ -1,28 +1,36 @@
 import type { RouteLocationNormalized } from 'vue-router'
-// import { useToast } from 'primevue/usetoast'
 import AuthProvider, { type IAuthProvider } from '~/resource/provider/Auth.provider'
 
 export default defineNuxtRouteMiddleware(async (to: RouteLocationNormalized): Promise<void> => {
-  if (to.path.startsWith('/public')) {
-    const authService: IAuthProvider = new AuthProvider()
-    const authStore = useAuthStore()
-    const { $handleLoading } = useNuxtApp()
-    // const toast = useToast()
+  const authService: IAuthProvider = new AuthProvider()
+  const authStore = useAuthStore()
+  const { $handleLoading } = useNuxtApp()
 
-    await $handleLoading(resetToken, {
-      // toast: {
-      //   instance: toast
-      // }
-    })
-    async function resetToken (): Promise<void> {
-      if (!authStore.userToken.refreshToken) return
-      const payload = {
-        refreshToken: authStore.userToken.refreshToken
-      }
-      const response = await authService.refreshToken(payload)
-      authStore.userToken.accessToken = response.accessToken
-      authStore.userToken.refreshToken = response.refreshToken
-      authStore.userToken.tokenExpireIn = response.tokenExpireIn
+  const isPublicPath = to.path.startsWith('/public')
+  const isHome = to.path === '/public/home'
+  const isLoggedIn = !!authStore.userToken.accessToken
+
+  // 🔒 ถ้าเป็น public แต่ไม่ใช่ home และยังไม่ได้ login → เด้ง
+  if (isPublicPath && !isHome && !isLoggedIn) {
+    return navigateTo('/auth/verify') as any
+  }
+
+  // 🔄 ถ้าเป็น public และ login แล้ว → refresh token
+  if (isPublicPath && isLoggedIn) {
+    await $handleLoading(resetToken)
+  }
+
+  async function resetToken (): Promise<void> {
+    if (!authStore.userToken.refreshToken) return
+
+    const payload = {
+      refreshToken: authStore.userToken.refreshToken
     }
+
+    const response = await authService.refreshToken(payload)
+
+    authStore.userToken.accessToken = response.accessToken
+    authStore.userToken.refreshToken = response.refreshToken
+    authStore.userToken.tokenExpireIn = response.tokenExpireIn
   }
 })
