5a3aace1d88a15f007856adbb7f3a47dbb7f0f9d
Punsano26 <654259026@webmail.npru.ac.th>
2026-04-01 16:52:02 +0700

    feat/add skeleton loading components and integrate into user profile and home pages

---
+++ app/components/skeleton/DataLoadingState.vue
@@ -0,0 +1,24 @@
+<template>
+  <template v-if="isLoading">
+    <slot name="skeleton">
+      <div
+        v-for="i in skeletonCount"
+        :key="`default-skeleton-${i}`"
+        class="animate-pulse bg-slate-200 h-20 w-full rounded" />
+    </slot>
+  </template>
+  <template v-else>
+    <slot />
+  </template>
+</template>
+
+<script setup lang="ts">
+interface Props {
+  isLoading: boolean
+  skeletonCount?: number
+}
+
+withDefaults(defineProps<Props>(), {
+  skeletonCount: 1
+})
+</script>
