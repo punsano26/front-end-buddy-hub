197a885a39563b5d60d2489e8490473edf8ce85a
Punsano26 <654259026@webmail.npru.ac.th>
2026-04-10 10:43:30 +0700

    feat/add chat provider createMessage and findOneMessagePaginate methods

---
+++ app/resource/provider/Chat.provider.ts
@@ -0,0 +1,27 @@
+import HttpRequest from '../HttpRequest'
+import type { ICreateMessagePayload, IFindOneMessagePaginateQuery } from '~/models/request/ChatReq.model'
+import type { TBaseParamsId } from '~/models/request/Request.model'
+import type { ICreateMessageResponse, IFindOneMessagePaginateResponse } from '~/models/response/ChatRes.model'
+
+export interface IChatProvider {
+  createMessage (payload: ICreateMessagePayload): Promise<ICreateMessageResponse>
+  findOneMessagePaginate (id: TBaseParamsId, query: IFindOneMessagePaginateQuery): Promise<IFindOneMessagePaginateResponse>
+}
+
+class ChatProvider extends HttpRequest implements IChatProvider {
+  private urlPrefix: string = '/chats'
+
+  public async createMessage (payload: ICreateMessagePayload): Promise<ICreateMessageResponse> {
+    this.setUserAuthHeader()
+    const response = await this.post(`${this.urlPrefix}/send`, payload)
+    return response
+  }
+
+  public async findOneMessagePaginate (id: TBaseParamsId, query: IFindOneMessagePaginateQuery): Promise<IFindOneMessagePaginateResponse> {
+    this.setUserAuthHeader()
+    const response = await this.get(`${this.urlPrefix}/messages/${id}`, query)
+    return response
+  }
+}
+
+export default ChatProvider
