c4ce4d7fcbb4a9734cb7f9ca471ccb8565ae753d
Punsano26 <654259026@webmail.npru.ac.th>
2026-04-14 21:59:27 +0700

    feat/update FriendProvider to return detailed responses for friend request operations

--- app/resource/provider/Friend.provider.ts
+++ app/resource/provider/Friend.provider.ts
@@ -1,18 +1,39 @@
 import HttpRequest from '../HttpRequest'
-import type { IFriendsIdRequest } from '~/models/request/Request.model'
-import type { IMessageResponse } from '~/models/response/Response.model'
+import type { IFindAllRequestPaginateQuery } from '~/models/request/FriendReq.model'
+import type { IFriendsIdRequest, TBaseParamsId } from '~/models/request/Request.model'
+import type { IFindAllRequestPaginateResponse, ISendAFriendRequestResponse } from '~/models/response/FriendRes.model'
 
 export interface IFriendProvider {
-  sendAFriendRequest (payload: IFriendsIdRequest): Promise<IMessageResponse>
-
+  sendAFriendRequest (payload: IFriendsIdRequest): Promise<ISendAFriendRequestResponse>
+  findAllRequestPaginate (query: IFindAllRequestPaginateQuery): Promise<IFindAllRequestPaginateResponse>
+  acceptFriendRequest (id: TBaseParamsId): Promise<ISendAFriendRequestResponse>
+  rejectFriendRequest (id: TBaseParamsId): Promise<ISendAFriendRequestResponse>
 }
 
 class FriendProvider extends HttpRequest implements IFriendProvider {
   private urlPrefix: string = '/friends'
 
-  public async sendAFriendRequest (payload: IFriendsIdRequest): Promise<IMessageResponse> {
+  public async sendAFriendRequest (payload: IFriendsIdRequest): Promise<ISendAFriendRequestResponse> {
     this.setUserAuthHeader()
     const response = await this.post(`${this.urlPrefix}/request`, payload)
+    return response
+  }
+
+  public async findAllRequestPaginate (query: IFindAllRequestPaginateQuery): Promise<IFindAllRequestPaginateResponse> {
+    this.setUserAuthHeader()
+    const response = await this.get(`${this.urlPrefix}/request/list`, query)
+    return response
+  }
+
+  public async acceptFriendRequest (id: TBaseParamsId): Promise<ISendAFriendRequestResponse> {
+    this.setUserAuthHeader()
+    const response = await this.patch(`${this.urlPrefix}/accept/${id}`, {})
+    return response
+  }
+
+  public async rejectFriendRequest (id: TBaseParamsId): Promise<ISendAFriendRequestResponse> {
+    this.setUserAuthHeader()
+    const response = await this.patch(`${this.urlPrefix}/reject/${id}`, {})
     return response
   }
 }
