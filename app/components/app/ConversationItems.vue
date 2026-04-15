58136abda2ffbbac809c36db63f7f3b07b9a1192
Punsano26 <654259026@webmail.npru.ac.th>
2026-04-15 23:37:28 +0700

    feat/conversation item layout with unread message badge and computed unread count

--- app/components/app/ConversationItems.vue
+++ app/components/app/ConversationItems.vue
@@ -2,8 +2,18 @@
   <NuxtLink
     :to="{ name: 'public-chat-id', params: { id: props.conversation.id } }"
     class="block w-full min-w-0">
-    <Card class="w-full max-w-full overflow-hidden">
+    <Card
+      class="w-full max-w-full overflow-hidden"
+      pt:body:class="pt-2">
       <template #content>
+        <div
+          v-if="unreadCount > 0"
+          class="flex justify-end">
+          <Badge
+            :value="unreadCount"
+            severity="success"
+            size="small" />
+        </div>
         <div class="flex items-center gap-3 min-w-0">
 
           <img
@@ -11,10 +21,10 @@
             src="/png/upload-profile.png">
 
           <div class="flex-1 min-w-0 overflow-hidden pr-2">
+
             <p class="font-semibold truncate">
               {{ props.conversation.nickname || props.conversation.username }}
             </p>
-
 
             <p class="text-sm text-gray-500 line-clamp-1">
               {{ props.conversation.lastMessage }}
@@ -34,10 +44,17 @@
 <script setup lang="ts">
 import dayjs from 'dayjs'
 import type { IFindAllConversationsList } from '~/models/response/ChatRes.model'
+import { useChatStore } from '~/stores/Chat'
 
 const props = defineProps<{
   conversation: IFindAllConversationsList
 }>()
+
+const chatStore = useChatStore()
+
+const unreadCount = computed((): number => {
+  return chatStore.unreadConversationCount(props.conversation.id)
+})
 </script>
 
 <style scoped>
