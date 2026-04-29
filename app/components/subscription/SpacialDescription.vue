54f7959200b42ffb4ee67e46593a2bc9ff909a38
Punsano26 <654259026@webmail.npru.ac.th>
2026-04-29 20:50:44 +0700

    feat/add SpacialDescription component for subscription details

---
+++ app/components/subscription/SpacialDescription.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="grid grid-cols-1 md:grid-cols-2 gap-2">
+    <div
+      v-for="(spacialDescription, index) in spacialDescriptions"
+      :key="index">
+      <Card pt:root:class="rounded-3xl">
+        <template #content>
+          <div class="flex gap-2 items-center">
+            <i :class="`pi ${spacialDescription.icon}`" />
+            <div class="flex flex-col gap-1">
+              <p>{{ spacialDescription.title }}</p>
+              <p class="text-sm text-gray-500">
+                {{ spacialDescription.description }}
+              </p>
+            </div>
+          </div>
+        </template>
+      </Card>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  spacialDescriptions: {
+    icon: string
+    title: string
+    description: string
+  }[]
+}>()
+</script>
+
+<style scoped>
+
+</style>
