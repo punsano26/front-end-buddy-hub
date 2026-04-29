a77ae1835fb3f111591c0c630da92bdb9747ba51
Punsano26 <654259026@webmail.npru.ac.th>
2026-04-29 20:50:33 +0700

    feat/add subscription package component

---
+++ app/components/subscription/Package.vue
@@ -0,0 +1,49 @@
+<template>
+  <Card>
+    <template #content>
+      <div class="flex flex-col gap-2 justify-center items-center">
+        <p>{{ package.name }}</p>
+        <div class="flex flex-col items-center">
+          <p class="text-2xl font-bold">
+            {{ package.price }} ฿
+          </p>
+          <p class="text-xs">
+            {{ package.smallDescription }}
+          </p>
+        </div>
+        <Divider />
+        <div class="flex flex-col gap-2">
+          <div
+            v-for="(feature, index) in package.features"
+            :key="index"
+            class="flex gap-2 items-center">
+            <i class="pi pi-check-circle text-green-500" />
+            <span class="text-sm">{{ feature }}</span>
+          </div>
+        </div>
+        <Button
+          :label="`สมัคร ${package.price} ฿`"
+          class="w-full"
+          severity="primary"
+          size="small" />
+      </div>
+    </template>
+  </Card>
+</template>
+
+<script lang="ts" setup>
+import Divider from '~/volt/Divider.vue'
+
+defineProps<{
+  package: {
+    name: string
+    price: number
+    smallDescription: string
+    features: string[]
+  }
+}>()
+</script>
+
+<style>
+
+</style>
