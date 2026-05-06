9cf56c267864f02c50555a8ed55e88bc83282ee7
Punsano26 <654259026@webmail.npru.ac.th>
2026-05-06 14:55:04 +0700

    feat/add RentFilter component and integrate it into the rent page

---
+++ app/pages/public/rent/index.vue
@@ -0,0 +1,55 @@
+<template>
+  <div
+    class="flex flex-col gap-2 mx-auto w-full max-w-7xl px-4 py-6 sm:px-6 sm:py-8 lg:px-10"
+  >
+    <h1 class="text-2xl font-bold sm:text-4xl">
+      <span
+        class="bg-gradient-to-r from-sky-500 to-indigo-600 bg-clip-text text-transparent"
+      >
+        เช่าเพื่อนคุย
+      </span>
+    </h1>
+    <div
+      class="flex flex-col gap-3 sm:flex-row sm:items-end sm:justify-between"
+    >
+      <p class="text-sm sm:max-w-2xl">
+        เลือกเพื่อนคุยที่ใช่ — คุยเล่นสนุกๆ
+        หรือปรึกษาปัญหาทางใจกับผู้รับฟังมืออาชีพ
+      </p>
+      <Button
+        label="เปิดเช่ารับฟัง"
+        icon="pi pi-plus"
+        pt:root:class="bg-gradient-primary border-none px-4 py-2 shadow-sm hover:shadow-md hover:scale-105 transition"
+        rounded
+      />
+    </div>
+    <div class="grid grid-cols-1 gap-1 sm:grid-cols-[1fr_auto] sm:items-center">
+      <InputSearch placeholder="ค้นหาชื่อ ความเชี่ยวชาญ..." class="w-full" />
+      <SelectInput
+        placeholder="เรตติ้งสูงสุด"
+        pt:root:class="w-full sm:w-48 shadow-sm"
+      />
+    </div>
+    <div class="flex justify-start">
+      <RentFilter/>
+    </div>
+
+    <div class="grid grid-cols-1 gap-2 sm:grid-cols-2 lg:grid-cols-3">
+      <RentCard />
+      <RentCard />
+      <RentCard />
+      <RentCard />
+      <RentCard />
+      <RentCard />
+      <RentCard />
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import ButtonGroup from '~/volt/ButtonGroup.vue'
+
+definePageMeta({ layout: "navbar" });
+</script>
+
+<style></style>
