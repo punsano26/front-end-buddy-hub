522048abbc27ffdfafcb0f6d046a398005adc2bb
Punsano26 <654259026@webmail.npru.ac.th>
2026-05-08 23:42:15 +0700

    fix: include email and verification status in user data computation

--- app/components/app/AvatarProfile.vue
+++ app/components/app/AvatarProfile.vue
@@ -37,6 +37,8 @@ const userData = computed((): IAuthLoginData => {
   return {
     id: authStore.user.id,
     username: authStore.user.username,
+    email: authStore.user.email,
+    isVerified: authStore.user.isVerified,
     roles: authStore.user.roles,
     profileImg: authStore.user.profileImg
   }
