8d1618b407011fe3bed5c26ca427879acc970bee
Punsano26 <654259026@webmail.npru.ac.th>
2026-05-19 18:28:32 +0700

    feat: add LeaveTheRandomMatchQueue functionality and response type

--- app/models/response/MatchRes.model.ts
+++ app/models/response/MatchRes.model.ts
@@ -4,4 +4,9 @@ export interface IJoinTheRandomMatchQueueData {
   status: string
 }
 
+export interface ILeaveTheRandomMatchQueueData {
+  removed: boolean
+}
+
+export type ILeaveTheRandomMatchQueueResponse = IApiResponse<ILeaveTheRandomMatchQueueData>
 export type IJoinTheRandomMatchQueueResponse = IApiResponse<IJoinTheRandomMatchQueueData>
--- app/resource/provider/Match.provider.ts
+++ app/resource/provider/Match.provider.ts
@@ -1,9 +1,10 @@
 import HttpRequest from '../HttpRequest'
 import type { IJoinTheRandomMatchQueuePayload } from '~/models/request/MatchReq.model'
-import type { IJoinTheRandomMatchQueueResponse } from '~/models/response/MatchRes.model'
+import type { IJoinTheRandomMatchQueueResponse, ILeaveTheRandomMatchQueueResponse } from '~/models/response/MatchRes.model'
 
 export interface IMatchProvider {
   JoinTheRandomMatchQueue (payload: IJoinTheRandomMatchQueuePayload): Promise<IJoinTheRandomMatchQueueResponse>
+  LeaveTheRandomMatchQueue (): Promise<ILeaveTheRandomMatchQueueResponse>
 }
 
 class MatchProvider extends HttpRequest implements IMatchProvider {
@@ -12,6 +13,12 @@ class MatchProvider extends HttpRequest implements IMatchProvider {
   public async JoinTheRandomMatchQueue (payload: IJoinTheRandomMatchQueuePayload): Promise<IJoinTheRandomMatchQueueResponse> {
     this.setUserAuthHeader()
     const response = await this.post(`${this.urlPrefix}/queue`, payload)
+    return response
+  }
+
+  public async LeaveTheRandomMatchQueue (): Promise<ILeaveTheRandomMatchQueueResponse> {
+    this.setUserAuthHeader()
+    const response = await this.delete(`${this.urlPrefix}/queue`)
     return response
   }
 }
