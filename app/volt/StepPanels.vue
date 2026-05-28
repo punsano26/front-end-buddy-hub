62a59178f4b39d596a9ea5e7fc9453c28decc759
punsano26 <654259026@webmail.npru.ac.th>
2026-05-28 17:32:17 +0700

    feat: add Step components and update ptViewMerge utility for improved styling

---
+++ app/volt/StepPanels.vue
@@ -0,0 +1,26 @@
+<template>
+  <StepPanels
+    :pt="theme"
+    :pt-options="{
+      mergeProps: ptViewMerge
+    }"
+    unstyled>
+    <slot />
+  </StepPanels>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+import { ptViewMerge } from './utils'
+import StepPanels, {
+  type StepPanelsPassThroughOptions,
+  type StepPanelsProps
+} from 'primevue/steppanels'
+
+interface Props extends /* @vue-ignore */ StepPanelsProps {}
+defineProps<Props>()
+
+const theme = ref<StepPanelsPassThroughOptions>({
+  root: `pt-[0.875rem] pb-[1.125rem] px-2`
+})
+</script>
