c70f3c9626fc70e9aad835a69ae6c5028238130c
punsano26 <654259026@webmail.npru.ac.th>
2026-06-01 20:30:21 +0700

    feat: add ButtonBack component to navbar and create public page with navbar layout

--- app/layouts/navbar.vue
+++ app/layouts/navbar.vue
@@ -4,6 +4,12 @@
       <ToolsBar />
     </div>
     <div class="pb-[calc(env(safe-area-inset-bottom)+5rem)] sm:pb-0">
+      <div class="mx-auto w-full max-w-7xl px-4 pt-4 sm:px-6 lg:px-10">
+        <ButtonBack
+          v-if="route.meta?.backPath && route.meta?.backPlacement !== 'page'"
+          :to="route.meta?.backPath"
+          icon="prime:chevron-left" />
+      </div>
       <slot />
     </div>
   </div>
@@ -11,6 +17,8 @@
 
 <script setup lang="ts">
 import ToolsBar from '~/components/app/ToolsBar.vue'
+
+const route = useRoute()
 </script>
 
 <style scoped>
