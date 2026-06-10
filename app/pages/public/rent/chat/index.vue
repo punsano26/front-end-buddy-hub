1349512a5acea02f74707b716a1fe56b09c74d6b
Punsano26 <654259026@webmail.npru.ac.th>
2026-06-10 17:16:13 +0700

    feat/create mockup UI for index chat not choose conversation

--- app/pages/public/rent/chat/index.vue
+++ app/pages/public/rent/chat/index.vue
@@ -1,13 +1,13 @@
 <template>
-    <div>
-
-    </div>
+  <div class="h-full flex items-center justify-center overflow-hidden">
+    <p class="hidden md:block text-gray-500">
+      Please select a conversation from the sidebar.
+    </p>
+  </div>
 </template>
 
 <script setup lang="ts">
-
+definePageMeta({ layout: "chat-rent", title: "แชท" });
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
