4688b147e613cbf713981151f2b367f62d72949e
Punsano26 <654259026@webmail.npru.ac.th>
2026-06-10 17:15:16 +0700

    feat/create chat page for id

--- app/pages/public/rent/chat/[id].vue
+++ app/pages/public/rent/chat/[id].vue
@@ -1,11 +1,11 @@
 <template>
     <div>
-
+        <h1>hello chat rent</h1>
     </div>
 </template>
 
 <script setup lang="ts">
-
+definePageMeta({ layout: "chat-rent", title: "แชท" });
 </script>
 
 <style scoped>
