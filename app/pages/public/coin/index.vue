d73187f8925e68a8dc953ecf575b75e317fefb0b
Punsano26 <654259026@webmail.npru.ac.th>
2026-06-18 16:29:35 +0700

    feat(coin): add coin index page

---
+++ app/pages/public/coin/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <CoinPackages />
+    <TransactionCoinHistory/>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import CoinPackages from '~/components/subscription/CoinPackages.vue'
+import TransactionCoinHistory from '~/components/subscription/TransactionCoinHistory.vue'
+</script>
+
+<style>
+
+</style>
