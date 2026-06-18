e3a5ad1d505a28366ebe16b3eace4f44ba4b9a38
Punsano26 <654259026@webmail.npru.ac.th>
2026-06-18 17:12:06 +0700

    feat(coin): update coin index page

--- app/pages/public/coin/index.vue
+++ app/pages/public/coin/index.vue
@@ -1,15 +1,20 @@
 <template>
-  <div>
+  <div class="mx-auto max-w-5xl px-4 py-8 space-y-10 sm:px-6">
     <CoinPackages />
-    <TransactionCoinHistory/>
+    <TransactionCoinHistory />
   </div>
 </template>
 
 <script lang="ts" setup>
 import CoinPackages from '~/components/subscription/CoinPackages.vue'
 import TransactionCoinHistory from '~/components/subscription/TransactionCoinHistory.vue'
+
+definePageMeta({
+  layout: 'navbar'
+})
+
+useSeoMeta({
+  title: 'กระเป๋าเหรียญ',
+  description: 'จัดการเหรียญสำหรับเช่าคุย ส่งของขวัญ และปลดล็อกฟีเจอร์พิเศษ'
+})
 </script>
-
-<style>
-
-</style>
