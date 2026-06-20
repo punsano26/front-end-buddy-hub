2f712847fcb0d063d53c4a1e0576911009c6e0e6
Punsano26 <654259026@webmail.npru.ac.th>
2026-06-21 02:25:19 +0700

    feat(rent): add IRentAPostPayload request type

--- app/models/request/RentReq.model.ts
+++ app/models/request/RentReq.model.ts
@@ -1,5 +1,10 @@
 import type { IBasePaginationRequest } from './Request.model'
 
+export interface IRentAPostPayload {
+  hirePostId: number
+  durationMinutes: number
+}
+
 export interface ICreateRentPostPayload {
   categoryId: number
   tagline: string
