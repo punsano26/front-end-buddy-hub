a8010671a2e8b83fc5359bf7ac73495a76019eaf
Punsano26 <654259026@webmail.npru.ac.th>
2026-06-23 13:19:16 +0700

    feat(rent): add accept and reject hire request API methods

--- app/resource/provider/RentCustomer.provider.ts
+++ app/resource/provider/RentCustomer.provider.ts
@@ -1,9 +1,12 @@
 import HttpRequest from '../HttpRequest'
 import type { IRentAPostPayload } from '~/models/request/RentReq.model'
+import type { TBaseParamsId } from '~/models/request/Request.model'
 import type { IRentAPostResponse } from '~/models/response/RentRes.model'
 
 export interface IRentCustomerProvider {
   rentAPost(payload: IRentAPostPayload): Promise<IRentAPostResponse>
+  acceptAHireRequest (id: TBaseParamsId): Promise<IRentAPostResponse>
+  rejectAHireRequest (id: TBaseParamsId): Promise<IRentAPostResponse>
 }
 
 class RentCustomerProvider extends HttpRequest implements IRentCustomerProvider {
@@ -12,6 +15,18 @@ class RentCustomerProvider extends HttpRequest implements IRentCustomerProvider 
   public async rentAPost (payload: IRentAPostPayload): Promise<IRentAPostResponse> {
     this.setUserAuthHeader()
     const response = await this.post(`${this.urlPrefix}`, payload)
+    return response
+  }
+
+  public async acceptAHireRequest (id: TBaseParamsId): Promise<IRentAPostResponse> {
+    this.setUserAuthHeader()
+    const response = await this.post(`${this.urlPrefix}/sessions/${id}/accept`, {})
+    return response
+  }
+
+  public async rejectAHireRequest (id: TBaseParamsId): Promise<IRentAPostResponse> {
+    this.setUserAuthHeader()
+    const response = await this.post(`${this.urlPrefix}/sessions/${id}/reject`, {})
     return response
   }
 }
