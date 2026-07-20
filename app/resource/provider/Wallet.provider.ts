befbadcf3827950aadb0f85c2b7b3b1ddd790b97
punsnao26 <654259026@webmail.npru.ac.th>
2026-07-20 15:43:36 +0700

    feat(wallet): implement sendCoinsToAnotherUser method in WalletProvider

--- app/resource/provider/Wallet.provider.ts
+++ app/resource/provider/Wallet.provider.ts
@@ -1,8 +1,10 @@
 import HttpRequest from '../HttpRequest'
-import type { IFindWalletBalanceResponse } from '~/models/response/WallRes.model'
+import type { ISendCoinsToAnotherUserPayload } from '~/models/request/WalletReq.model'
+import type { IFindWalletBalanceResponse, ISendCoinsToAnotherUserResponse } from '~/models/response/WallRes.model'
 
 export interface IWalletProvider {
   findWalletBalance (): Promise<IFindWalletBalanceResponse>
+  sendCoinsToAnotherUser (payload: ISendCoinsToAnotherUserPayload): Promise<ISendCoinsToAnotherUserResponse>
 }
 
 class WalletProvider extends HttpRequest implements IWalletProvider {
@@ -11,6 +13,12 @@ class WalletProvider extends HttpRequest implements IWalletProvider {
   public async findWalletBalance (): Promise<IFindWalletBalanceResponse> {
     this.setUserAuthHeader()
     const response = await this.get(`${this.urlPrefix}`)
+    return response
+  }
+
+  public async sendCoinsToAnotherUser (payload: ISendCoinsToAnotherUserPayload): Promise<ISendCoinsToAnotherUserResponse> {
+    this.setUserAuthHeader()
+    const response = await this.post(`${this.urlPrefix}/transfer`, payload)
     return response
   }
 }
