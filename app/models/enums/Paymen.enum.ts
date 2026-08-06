0cb047a4e92ac1bfdec4d1a425ebe2b2923d6faa
punsnao26 <654259026@webmail.npru.ac.th>
2026-08-06 17:05:32 +0700

    feat(payment): add PaymentStatusEnum for tracking payment statuses

--- app/models/enums/Paymen.enum.ts
+++ app/models/enums/Paymen.enum.ts
@@ -2,3 +2,11 @@ export enum PaymentMethodEnum {
   PROMPTPAY = 'promptpay',
   CREDIT_CARD = 'card'
 }
+
+export enum PaymentStatusEnum {
+  PENDING = 'pending',
+  SUCCEEDED = 'succeeded',
+  FAILED = 'failed',
+  CANCELLED = 'cancelled',
+  EXPIRED = 'expired'
+}
