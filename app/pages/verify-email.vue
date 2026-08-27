0fb3f91889d286dc0599156470e637e74f426e3d
punsnao26 <654259026@webmail.npru.ac.th>
2026-08-27 21:37:36 +0700

    feat(VerifyEmail): add verify email page with routing logic

---
+++ app/pages/verify-email.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="min-h-screen flex items-center justify-center">
+    <i class="pi pi-spin pi-spinner text-4xl text-primary" />
+  </div>
+</template>
+
+<script setup lang="ts">
+const route = useRoute()
+const router = useRouter()
+
+onBeforeMount((): void => {
+  router.replace({
+    name: 'auth-verify-email',
+    query: route.query
+  })
+})
+</script>
+
+<style scoped>
+</style>
